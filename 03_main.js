85d4cfaac38d2ece759739c3795843f537df0007
Cser <cser.hajnalka@petrik.hu>
2023-09-14 20:22:26 +0200

    függvenyek

--- 03_main.js
+++ 03_main.js
@@ -9,3 +9,25 @@ function villanykörteLekapcsolása(params) {
     console.log(villanykörte);
     villanykörte.setAttribute("src", "img/pic_bulboff.gif");
 }
+
+function bevitelFocus(){
+    console.log("Belekattintott a beviteli mezőre")
+}
+
+
+function bevitelInput(){
+    const bevitel= document.getElementById("bevitel");
+    const beirtErtek= bevitel.value;
+    console.log("Mezőbe írt érték:", beirtErtek);
+}
+
+function bevitelBlur(){
+    const bekezdes= document.getElementById("üzenet");
+    bekezdes.innerHTML ="Nyisd meg a konzolt";    
+}
+
+function uzenetPirosra(){
+    const uzenet =document.getElementById("uzenet");
+    uzenet.style.color="red";
+    uzenet.style.backgroundColor="#AAAA";
+}
