2fa6ce04d2ee162c40302719391166291343ff53
Cser <cser.hajnalka@petrik.hu>
2023-09-14 19:53:37 +0200

    Különböző Js események

---
+++ 03_main.js
@@ -0,0 +1,11 @@
+function villanykörteFelkapcsolása(params) {
+    const villanykörte= document.getElementById("villanykörte");
+    console.log(villanykörte);
+    villanykörte.setAttribute("src", "img/pic_bulbon.gif");
+}
+
+function villanykörteLekapcsolása(params) {
+    const villanykörte= document.getElementById("villanykörte");
+    console.log(villanykörte);
+    villanykörte.setAttribute("src", "img/pic_bulboff.gif");
+}
